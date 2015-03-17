c2b2f481694eab910bfe559857efcb7ade1d4231
seiyavogt <seiya@Seiyas-MacBook-Air.local>
2015-03-17 16:59:43 -0400

    Changed js and css

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,61 @@
 jQuery( document ).ready(function( $ ) {
-    // variable to hold request
 
-    var request;
 
+  var participants_selector = $("#number-of-participants"); 
+  var entry_details_area = $("#game-data");
+
+  //Each time a new value is chosen, grab their number of participants
+  participants_selector.on("change", function()
+  {
+    //Get the number of players
+    var number_of_participants = participants_selector.val();
+    
+    //Clear out any previously-appended HTML
+    entry_details_area.empty();
+    
+    for (var i = 0; i < Number(number_of_participants); i++)
+    {
+      
+      var participant_number = i+1; 
+      
+      var entry_form_html = [
+            '<form id="game1" class="game"><input ',
+            '            type="text" ',
+            '            name="Name"',
+            '            placeholder="Enter a Name"',
+            '            class="nameform"',
+            '            id="Name">',
+            '        <select name="Role">',
+            '  <option value="None">None</option>',
+            '  <option value="Assassin">Assassin</option>',
+            '  <option value="Merlin">Merlin</option>',
+            '  <option value="Mordred">Mordred</option>',
+            '  <option value="Morgana">Morgana</option>',
+            '  <option value="Oberon">Oberon</option>',
+            '  <option value="Percival">Percival</option>',
+            '</select>',
+            '      <select name="Alignment">',
+            '  <option value="Good">Good</option>',
+            '  <option value="Evil">Evil</option>',
+            '</select>',
+            '      <select name="Result">',
+            '  <option value="Win">Win</option>',
+            '  <option value="Loss">Loss</option>',
+            '</select>',
+            '</form>',
+            ''
+            ].join('');
+                            
+      //Add the form for each player                      
+      entry_details_area.append(entry_form_html);
+    }
 
     $(".game").submit(function(event){
         // if (request) {
         //     request.abort();
         // }
+         // variable to hold request
+        var request;
         var $form = $(this);
         var $inputs = $form.find("input, select, button, textarea, radio");
         var gameResult = $form.serialize();
@@ -44,18 +92,25 @@ jQuery( document ).ready(function( $ ) {
     
         event.preventDefault();
     });
-
+  });
 
 //trigger multiple form submits with one button
 
+// $("#submitbutton").click(function(){
+//     $("#game-data").find('.game').each(function(){
+//         $(this).trigger("submit");
+//     });
+// });
+
 $("#submitbutton").click(function(){
-    $(".game").each(function(){
-        $(this).trigger("submit");
-    });
+  console.log($("#game-data").find('.game'));
+   $("#game-data").find('.game').each(function(){
+       $(this).trigger("submit");
+   });
 });
 
+    
+
 });
-
-
 
 
